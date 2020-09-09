8a51dcec40987ae49c15c3d4cab5ee46ca36c04c
Ari Putra <ariteknologi@gmail.com>
2020-09-10 02:23:13 +0700

    add browser router to primary component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import configureStore from './store'
+import { BrowserRouter } from "react-router-dom";
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>, 
 document.getElementById('root'));

ad711ccb7805a313cae393739008e22863f1057c
Ari Putra <ariteknologi@gmail.com>
2020-09-10 02:29:31 +0700

    add bootstrap package

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux'
 import configureStore from './store'
 import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 
